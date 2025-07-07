2536cb0505f8bd3253fa8419aa0ae115fe2b991d
Pong <tammasorn9874@gmail.com>
2025-07-07 10:35:32 +0700

    setup i18n

---
+++ libs/shared/i18n/README.md
@@ -0,0 +1,7 @@
+# @myorg/shared-i18n
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @myorg/shared-i18n` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ libs/shared/ui/src/lib/ui.tsx
@@ -0,0 +1,11 @@
+import styles from './ui.module.css';
+
+export function QreactUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to QreactUi!</h1>
+    </div>
+  );
+}
+
+export default QreactUi;
